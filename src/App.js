461b175fa1f0f557bb5cf72ea3f42f319e9ccd79
rawvie <rawvie@ngit.com.np>
2024-05-29 16:34:20 +0545

    all commits batman

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Lucid, Blockfrost } from "lucid-cardano";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [cbor, setCbor] = useState("");
+
+	const signTransaction = async () => {
+		const lucid = await Lucid.new(
+			new Blockfrost(
+				"https://cardano-preview.blockfrost.io/api/v0",
+				"previewWenCzTRwLmYSJuTHiZv5yiqdkJ7l2Iih"
+			),
+			"Preview"
+		);
+		const api = await window.cardano.nami.enable();
+		lucid.selectWallet(api);
+		const unsigned = await lucid
+			.newTx()
+			.payToAddress(
+				"addr_test1qppavs25cr2fn57e4uszpcv06y7pkvjcsczju3j8p4s9543kfglnwy0f3fh3mrv9hc8hr844wa668cgyvxs5qxuxwuhs6ehze4",
+				{ '8d2a90fab86ad0869ce94cbaad93a1217bc2f2bafb84e3bc27b5b92d': 500n }
+			)
+			.complete().catch(err => console.log({ err }));
+
+		console.log({ unsigned })
+		const signed = await unsigned.sign().complete();
+		const hash = await signed.submit();
+		setCbor(hash);
+	};
+	return (
+		<div className="App">
+			<button onClick={async () => {
+				try {
+					await signTransaction()
+				} catch (error) {
+					console.error(error)
+				}
+			}}>Sign transaction</button>
+			<br />
+			<textarea value={cbor} />
+		</div>
+	);
 }
 
 export default App;
